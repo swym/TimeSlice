495cdf0ba31ab42aee2f7283eed27940f053fcc3
Alexander Mertens <alexander.mertens@me.com>
2014-04-11 18:44:07 +0200

    User interface prints now the remaining time in big nice letters.

--- src/timeslice/UserInterface.py
+++ src/timeslice/UserInterface.py
@@ -20,8 +20,10 @@ class UserInterface(object):
         self._session = Session(title, durationActive, durationPause)
         self._screen = None
         self._run_screen = True
+        self._big_numbers_dict = None
         
         self._init_screen()
+        self._init_big_numbers_dict()
         self._start_screen()
 
 
@@ -35,13 +37,106 @@ class UserInterface(object):
 
         # input with getch() shall not block
         self._screen.nodelay(True)
+        
+    def _init_big_numbers_dict(self):
+        
+        self._big_numbers_dict = dict()
+
+        big_number = []
+        big_number.append("  ___  ")
+        big_number.append(" / _ \ ")
+        big_number.append("| | | |")
+        big_number.append("| |_| |")
+        big_number.append(" \___/ ")
+        self._big_numbers_dict["0"] = big_number
+    
+        big_number = []
+        big_number.append(" _ ")
+        big_number.append("/ |")
+        big_number.append("| |")
+        big_number.append("| |")
+        big_number.append("|_|")
+        self._big_numbers_dict["1"] = big_number
+    
+        big_number = []
+        big_number.append(" ____  ")
+        big_number.append("|___ \ ")
+        big_number.append("  __) |")
+        big_number.append(" / __/ ")
+        big_number.append("|_____|")
+        self._big_numbers_dict["2"] = big_number
+    
+        big_number = []
+        big_number.append(" _____ ")
+        big_number.append("|___ / ")
+        big_number.append("  |_ \ ")
+        big_number.append(" ___) |")
+        big_number.append("|____/ ")
+        self._big_numbers_dict["3"] = big_number
+    
+        big_number = []
+        big_number.append(" _  _   ")
+        big_number.append("| || |  ")
+        big_number.append("| || |_ ")
+        big_number.append("|__   _|")
+        big_number.append("   |_|  ")
+        self._big_numbers_dict["4"] = big_number
+    
+        big_number = []
+        big_number.append(" ____  ")
+        big_number.append("| ___| ")
+        big_number.append("|___ \ ")
+        big_number.append(" ___) |")
+        big_number.append("|____/ ")
+        self._big_numbers_dict["5"] = big_number
+    
+        big_number = []
+        big_number.append("  __   ")
+        big_number.append(" / /_  ")
+        big_number.append("| '_ \ ")
+        big_number.append("| (_) |")
+        big_number.append(" \___/ ")
+        self._big_numbers_dict["6"] = big_number
+          
+        big_number = []
+        big_number.append(" _____ ")
+        big_number.append("|___  |")
+        big_number.append("   / / ")
+        big_number.append("  / /  ")
+        big_number.append(" /_/   ")
+        self._big_numbers_dict["7"] = big_number
+    
+        big_number = []
+        big_number.append("  ___  ")
+        big_number.append(" ( _ ) ")
+        big_number.append(" / _ \ ")
+        big_number.append("| (_) |")
+        big_number.append(" \___/ ")
+        self._big_numbers_dict["8"] = big_number
+    
+        big_number = []
+        big_number.append("  ___  ")
+        big_number.append(" / _ \ ")
+        big_number.append("| (_) |")
+        big_number.append(" \__, |")
+        big_number.append("   /_/ ")
+        self._big_numbers_dict["9"] = big_number
+        
+        big_number = []
+        big_number.append("   ")
+        big_number.append(" _ ")
+        big_number.append("(_)")
+        big_number.append(" _ ")
+        big_number.append("(_)")
+        self._big_numbers_dict[":"] = big_number
 
     def _start_screen(self):
 
         while self._run_screen:
+            
             self._refresh_screen()
-            self._get_user_input()            
-            curses.napms(100)
+            self._get_user_input()           
+            curses.napms(125)
             
     def _refresh_screen(self):
         self._screen.clear()
@@ -63,30 +158,30 @@ class UserInterface(object):
                 ts_state = "cancelled"
 
             # TODO: implement session-getter for remaining time
-            ts_duration = "duration: "
-            ts_duration += str(self._session.get_current_timeslice().get_remaining_time())
+            ts_duration = str(self._session.get_current_timeslice().get_remaining_time())
 
             self._screen.addstr(1, 2, "Title: " + ts_title)
             self._screen.addstr(2, 2, "State: " + ts_state)
-            self._screen.addstr(4, 2, ts_duration)
-            self._screen.addstr(6, 2, "Interruptions:")
-            self._screen.addstr(7, 4, "internal: " + str(self._session.get_current_timeslice_internal_interruptions()))
-            self._screen.addstr(8, 4, "external: " + str(self._session.get_current_timeslice_external_interruptions()))
+            self._screen.addstr(3, 2, "duration: " + ts_duration)
+            self._add_big_numbers(4, 2, ts_duration.partition('.')[0])
+            self._screen.addstr(10, 2, "Interruptions:")
+            self._screen.addstr(11, 4, "internal: " + str(self._session.get_current_timeslice_internal_interruptions()))
+            self._screen.addstr(12, 4, "external: " + str(self._session.get_current_timeslice_external_interruptions()))
 
 
             # options
             if self._session.is_current_timeslice_inited():
-                self._screen.addstr(10, 4, "s - start")
+                self._screen.addstr(13, 4, "s - start")
             elif self._session.is_current_timeslice_running():
-                self._screen.addstr(10, 4, "i - internal interruption")
-                self._screen.addstr(11, 4, "e - external interruption")
-                self._screen.addstr(13, 4, "c - cancel")
+                self._screen.addstr(13, 4, "i - internal interruption")
+                self._screen.addstr(14, 4, "e - external interruption")
+                self._screen.addstr(15, 4, "c - cancel")
             elif (self._session.is_current_timeslice_completed() or
                   self._session.is_current_timeslice_cancelled()):
-                self._screen.addstr(10, 4, "n - next")
+                self._screen.addstr(13, 4, "n - next")
 
-            self._screen.addstr(15, 4, "q - quit session")
-            self._screen.addstr(16, 4,"") # Cursor for user input
+            self._screen.addstr(18, 4, "q - quit session")
+            self._screen.addstr(19, 4,"") # Cursor for user input
         else:
             self._screen.addstr(2, 4, "session queue empty - good job! :-)")
             self._screen.addstr(5, 4, "q - quit session")
@@ -106,6 +201,15 @@ class UserInterface(object):
         elif c == ord("q"):
             self._run_screen = False
             
-    def _print_big_number(self, number_str = "1"):
+    def _add_big_numbers(self, row, col, number_string):
         
-        pass
+        # iterate over every char in string
+        for char in number_string:
+            #look up char in dictonary
+            if char in self._big_numbers_dict:
+                #if found
+                for i in range(0, len(self._big_numbers_dict[char])):
+                    #add number row by row to screen
+                    self._screen.addstr(row + i, col, self._big_numbers_dict[char][i])
+                # move col-pointer directly behind last printed number
+                col = col + len(self._big_numbers_dict[char][0])
